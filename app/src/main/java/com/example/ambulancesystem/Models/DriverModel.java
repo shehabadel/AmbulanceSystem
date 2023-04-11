35ecf06bdfd10ac1928ceb32983a3545a2c3023d
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-04-11 15:54:45 +0200

    Created DriverModel

--- app/src/main/java/com/example/ambulancesystem/Models/DriverModel.java
+++ app/src/main/java/com/example/ambulancesystem/Models/DriverModel.java
@@ -1,4 +1,96 @@
 package com.example.ambulancesystem.Models;
 
-public class DriverModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class DriverModel implements Parcelable {
+    String driverName;
+    String driverCarNumber;
+    String driverPhoneNumber;
+    String driverEstimatedTime;
+    Location driverLocation;
+
+    public DriverModel(String driverName, String driverCarNumber, String driverPhoneNumber, String driverEstimatedTime, Location driverLocation) {
+        this.driverName = driverName;
+        this.driverCarNumber = driverCarNumber;
+        this.driverPhoneNumber = driverPhoneNumber;
+        this.driverEstimatedTime = driverEstimatedTime;
+        this.driverLocation = driverLocation;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getDriverCarNumber() {
+        return driverCarNumber;
+    }
+
+    public void setDriverCarNumber(String driverCarNumber) {
+        this.driverCarNumber = driverCarNumber;
+    }
+
+    public String getDriverPhoneNumber() {
+        return driverPhoneNumber;
+    }
+
+    public void setDriverPhoneNumber(String driverPhoneNumber) {
+        this.driverPhoneNumber = driverPhoneNumber;
+    }
+
+    public String getDriverEstimatedTime() {
+        return driverEstimatedTime;
+    }
+
+    public void setDriverEstimatedTime(String driverEstimatedTime) {
+        this.driverEstimatedTime = driverEstimatedTime;
+    }
+
+    public Location getDriverLocation() {
+        return driverLocation;
+    }
+
+    public void setDriverLocation(Location driverLocation) {
+        this.driverLocation = driverLocation;
+    }
+
+    protected DriverModel(Parcel in) {
+        driverName = in.readString();
+        driverCarNumber = in.readString();
+        driverPhoneNumber = in.readString();
+        driverEstimatedTime = in.readString();
+        driverLocation = in.readParcelable(Location.class.getClassLoader());
+    }
+
+    public static final Creator<DriverModel> CREATOR = new Creator<DriverModel>() {
+        @Override
+        public DriverModel createFromParcel(Parcel in) {
+            return new DriverModel(in);
+        }
+
+        @Override
+        public DriverModel[] newArray(int size) {
+            return new DriverModel[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(driverName);
+        dest.writeString(driverCarNumber);
+        dest.writeString(driverPhoneNumber);
+        dest.writeString(driverEstimatedTime);
+        dest.writeParcelable(driverLocation, flags);
+    }
 }
