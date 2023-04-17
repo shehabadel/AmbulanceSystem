646ee6c8641582404c579d87b240bec529b30d9f
Omar ElKordy <omarelkordy9@gmail.com>
2023-04-17 23:37:43 +0200

    Adding Profile Activity

---
+++ app/src/main/java/com/example/ambulancesystem/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.ambulancesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        backButton = (ImageView) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
