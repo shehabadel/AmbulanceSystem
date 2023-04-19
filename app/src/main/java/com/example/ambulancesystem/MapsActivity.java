cc0428182749e5cb95af653ec3a87db51ab477c4
FarahEssamm <farahessam90@yahoo.com>
2023-04-19 14:53:35 +0200

    Maps
    distance between 2 fixed points

---
+++ app/src/main/java/com/example/ambulancesystem/MapsActivity.java
@@ -0,0 +1,67 @@
+package com.example.ambulancesystem;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.ambulancesystem.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng Hospital = new LatLng(30.100435, 31.372913);
+        mMap.addMarker(new MarkerOptions().position(Hospital).title("Marker in Nozha International Hospital"));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
+
+        double lat1 = 30.100435;
+        double long1 = 31.372913;
+
+        Location location1 = new Location("");
+        location1.setLatitude(lat1);
+        location1.setLongitude(long1);
+
+        double lat2 = 30.064545;
+        double long2 = 31.278596;
+
+        Location location2 = new Location("");
+        location2.setLatitude(lat2);
+        location2.setLongitude(long2);
+
+        float distanceInMeters = location1.distanceTo(location2);
+    }
+}
