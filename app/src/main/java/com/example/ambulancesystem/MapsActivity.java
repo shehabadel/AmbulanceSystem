f7df6ae9971dbfe59009ad11f223937207547afd
Mostafa Shams <mosstafashamss@gmail.com>
2023-04-29 23:51:32 +0200

    Activating viewBinding

--- app/src/main/java/com/example/ambulancesystem/MapsActivity.java
+++ app/src/main/java/com/example/ambulancesystem/MapsActivity.java
@@ -1,17 +1,17 @@
 package com.example.ambulancesystem;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.ambulancesystem.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.ambulancesystem.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
